e5ec75cbdea854161d7f947e58352d4c829187d2
suvar <suvar@LAPTOP-1C2KTHSV>
2021-09-07 20:15:15 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello La");
+            int toplam = SayilariTopla(5, 3);
+            Console.WriteLine(toplam);
+            Console.Read();
+
+
+        }
+
+        static int SayilariTopla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+    }
+}
